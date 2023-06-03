ed0bce5b3b44af53a30007283ef289ace7bb5c97
Tushar Rawat <tusharrawat52@gmail.com>
2023-06-03 22:02:10 +0530

    delete posts

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 const passport = require('passport');
 
 module.exports.create = (req,res)=>{
@@ -13,4 +14,45 @@ module.exports.create = (req,res)=>{
         console.log("error in posting",error);
         return res.redirect('back');
     })
+}
+
+
+
+
+module.exports.destroy = (req,res)=>{
+
+     Post.findById(req.params.id)
+     .then((post)=>{
+          if(post){
+
+            // I cant delete anyone's posts ----- post.user => id of the post creater
+            // .id means converting the object id into string
+            if(post.user == req.user.id){
+                post.deleteOne()
+
+                Comment.deleteMany({commentOnPost : req.params.id})
+                .then(()=>{
+                    return res.redirect('back');
+                })
+                .catch((err)=>{
+                    console.log("error is deleteing comments",err);
+                    return res.redirect('back');
+                })
+
+            }
+            else{
+                console.log('User not authorized to delete post');
+                return res.redirect('back');
+            }
+
+          }
+          else{
+            console.log("Post not found");
+            return res.redirect('back');
+          }
+     })
+     .catch((err)=>{
+        console.log("error in finding post",err);
+        return red.redirect('back');
+     })
 }
